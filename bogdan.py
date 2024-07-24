251fea44c9cd803cbfbe0b98201bd1f7e8feee5d
Maksim Levanovich <levanovichmaksim35@gmail.com>
2024-07-24 13:01:49 -0500

    paused

--- bogdan.py
+++ bogdan.py
@@ -5510,4 +5510,25 @@ print(first_comment.total_comments)  # 10
 # выше их можно изменять непоследственно в одной из функции непосредственно
 # в классе - это допускается.
 
-"Hello world"
+# Магические мтеоды в классах.
+
+
+class Comment:
+    def __init__(self, text):
+        self.text = text
+        self.votes_qty = 0
+
+    def upvote(self):
+        self.votes_qty += 1
+
+    def __add__(self, other):
+        return (f"{self.text} {other.text}",
+                self.votes_qty + other.votes_qty)
+
+
+firs_comment = Comment("First comment")
+firs_comment.upvote()
+second_comment = Comment("Second comment")
+second_comment.upvote()
+
+# Остановился здесь Раздел 41: Магические методы.
