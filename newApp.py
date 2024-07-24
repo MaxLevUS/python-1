8fda8dd2cbca70d7b9e1e959e4dbef62eae23200
Maksim Levanovich <levanovichmaksim35@gmail.com>
2024-07-24 12:29:29 -0500

    bogdan_python

---
+++ newApp.py
@@ -0,0 +1,21 @@
+"""test = 150;
+print(test)
+
+test = 500;
+print(test)"""
+
+"""import random
+rundomNum = random.randint(10, 100)
+
+user_number = input("Insert the number")
+
+
+if user_number == rundomNum:
+    print("Your guess is correct")
+    print(f"The number is {rundomNum}")
+else:
+    print("Your didn't guess the number")
+    print(f"Then number is {rundomNum}")"""
+
+
+print("Maksim")
